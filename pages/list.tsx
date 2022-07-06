815b49f8cea32bcf44c8fce31a0021b60361d7dd
Tomaz Jejcic <tomaz.jejcic@gmail.com>
2022-07-06 09:23:14 +0200

    add vanilla extract

---
+++ pages/list.tsx
@@ -0,0 +1,9 @@
+import { themeClass, exampleStyle } from '../styles/styles.css';
+
+export default function List() {
+    return (
+        <h1 className={exampleStyle}>
+            the list
+        </h1>
+    )
+}
